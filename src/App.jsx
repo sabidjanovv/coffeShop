63b886d3da7e27eda3970796875921ac4c962b52
sabidjanovv <sabidjanov@gmail.com>
2024-11-24 10:23:12 +0500

    Finally

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
 import Features from "./components/features/Features";
-// import ProductGrid from "./components/productGrid/ProductGrid";
-import FeatureSection from "./components/featureSection/FeatureSection";
 
 import "./App.css";
 
@@ -13,13 +11,6 @@ const App = () => {
       <Header />
       <Hero />
       <Features />
-      <section className="popular-products">
-        <h2 className="section-title">Popular Product</h2>
-        <h3 className="section-subtitle">Coffee Popular Product</h3>
-        <ProductGrid />
-        <button className="view-all-btn">View All Product</button>
-      </section>
-      <FeatureSection />
     </div>
   );
 };
